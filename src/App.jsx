08318c2091f5f76ac896ccb7cf41ef00f107b54e
leshfayisa <shambelfayisa76@gmail.com>
2025-06-15 13:08:59 +0300

    Initial commit: working Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-green-400 underline">
+        Tailwind colors are now working!
+      </h1>
+    </div>
+  );
+}
+
+export default App
